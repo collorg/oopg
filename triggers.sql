011fe38b519f782506e95199c38346cf70eee5ad
Joël Maïzi <joel.maizi@lirmm.fr>
2015-12-09 12:55:27 +0100

    Unification insert et update.

--- triggers.sql
+++ triggers.sql
@@ -4,11 +4,10 @@ create language plpythonu;
 --
 --
 
-CREATE FUNCTION before_insert_tuple() RETURNS trigger AS $$
+CREATE FUNCTION check_pk() RETURNS trigger AS $$
 oid = TD['relid']
-fqtn = plpy.execute("""SELECT get_fqtn('%s')""" % oid)[0]['get_fqtn']
 GD['td'] = TD
-plpy.execute("SELECT check_uid('%s', '%s', '%s')" % (oid, fqtn, oid))
+plpy.execute("SELECT check_pk_oid('{}', '{}')".format(oid, oid))
 $$
 LANGUAGE plpythonu;
 
@@ -16,23 +15,7 @@ LANGUAGE plpythonu;
 --
 --
 
-CREATE FUNCTION before_update_tuple() RETURNS trigger AS $$
-from mx import DateTime
-
-oid = TD['relid']
-old_uid = TD['old']['tpl_uid']
-fqtn = TD['old']['tpl_fqtn']
-changeset = TD['old']['tpl_changeset'] + 1
-GD['td'] = TD
-rec = plpy.execute("SELECT check_uid('%s', '%s', '%s')" % (oid, fqtn, oid))
-$$
-LANGUAGE plpythonu;
-
---
--- Name: check_uid(integer, text, integer); Type: FUNCTION; Schema: public; Owner: postgres
---
-
-CREATE FUNCTION check_uid(integer, text, integer) RETURNS text AS $$
+CREATE FUNCTION check_pk_oid(integer, integer) RETURNS text AS $$
 """
 recherche d''éventuelles correspondances dans l'arbre d''héritage.
 Si une correspondance est trouvée, déclenche une erreur.
@@ -40,31 +23,29 @@ Sinon retourne l'uid correspondants à la concaténation des champs de la clef
 primaire et du fqtn à gauche et à droite deux fois signés sha.
 """
 import sys
-import sha
+from psycopg2.extensions import adapt
 
 new_uid = ''
 #inhf = False
 oid_orig = args[0]
-fqtn_orig = args[1]
-oid = args[2]
-sys.stderr.write("{} check_uid({}, {}, {})\n".format(
-    8*'=', oid_orig, fqtn_orig, oid))
+oid = args[1]
+sys.stderr.write("{} check_pk_oid({}, {})\n".format(
+    8*'=', oid_orig, oid))
 TD = GD['td']
 sys.stderr.write("GD['td'] = {}\n".format(TD))
 parent_oid = plpy.execute(
-    "SELECT get_inhparent('%s')" % oid)[0]['get_inhparent']
+    "SELECT get_inhparent('{}')".format(oid))[0]['get_inhparent']
 sys.stderr.write("oid du parent {}\n".format(parent_oid))
 if parent_oid:
    # si la table hérite d''une autre table, on recherche l''existence
    # d''un tuple correspondant dans la table mère
-   query = ("SELECT check_uid('%s', '%s', '%s')" % (
-      oid_orig, fqtn_orig, parent_oid))
+   query = ("SELECT check_pk_oid('{}', '{}')".format(oid_orig, parent_oid))
    sys.stderr.write("check uid request: {}\n".format(query))
    rec = plpy.execute(query)
-   new_uid = rec[0]['check_uid']
+   new_uid = rec[0]['check_pk_oid']
 # récupration du fqtn et des champs de la clef primaire
 pk_infos = plpy.execute(
-   """SELECT get_pk_fields(%s)""" % (oid))[0]['get_pk_fields']
+   "SELECT get_pk_fields({})".format(oid))[0]['get_pk_fields']
 fqtn, pk_fieldnames = pk_infos.split(':')
 clause = []
 if not pk_fieldnames:
@@ -74,20 +55,16 @@ pklist = pk_fieldnames.split(',')
 # construction de la clause pour la requête SELECT
 l_pk_val = []
 for field in pklist:
-   if field == 'tpl_fqtn':
-      TD['new'][field] = fqtn_orig
    if TD['new'][field] == 0:
      valeur = 0
    else:
      valeur = TD['new'][field] or ""
-   valeur = str(valeur).replace("'", "''")
-   valeur = valeur.replace("\\", "\\\\")
-   l_pk_val.append(field + ":" + valeur)
-   clause.append(field + " = '" + valeur + "'")
+     valeur = adapt(valeur)
+   clause.append("{} = {}".format(field, str(valeur)))
 
 # construction de la requête d''extraction
 req = "SELECT * FROM {} WHERE {} limit 1".format(fqtn, ' and '.join(clause))
-sys.stderr.write("check_uid: {}\n".format(req))
+sys.stderr.write("check_pk_oid: {}\n".format(req))
 if len(plpy.execute(req)) == 1:
     plpy.error("clef dupliquee")
 
@@ -95,58 +72,48 @@ return 'ok'
 $$
 LANGUAGE plpythonu;
 
-ALTER FUNCTION check_uid(integer, text, integer) OWNER TO postgres;
-
 --
--- Name: get_fqtn(integer); Type: FUNCTION; Schema: public; Owner: postgres
+--
 --
 
-CREATE FUNCTION get_fqtn(integer) RETURNS text
-    AS $$
+CREATE FUNCTION get_fqtn(integer) RETURNS text AS $$
 import sys
 oid = args[0]
 sys.stderr.write("{} get_inhparent({})\n".format(8*'=', oid))
 rec = plpy.execute(
    """SELECT schemaname, relname      
       FROM pg_catalog.pg_stat_user_tables
-      WHERE relid = '%s'
-   """ % (oid))
+      WHERE relid = '{}'
+   """.format(oid))
 return rec[0]['schemaname'] + "." + rec[0]['relname']
 $$
-    LANGUAGE plpythonu;
-
-
-ALTER FUNCTION get_fqtn(integer) OWNER TO postgres;
+LANGUAGE plpythonu;
 
 --
--- Name: get_inhparent(integer); Type: FUNCTION; Schema: public; Owner: postgres
+--
 --
 
-CREATE FUNCTION get_inhparent(integer) RETURNS integer
-    AS $$
+CREATE FUNCTION get_inhparent(integer) RETURNS integer AS $$
 import sys
 relid = args[0]
 sys.stderr.write("{} get_inhparent({})\n".format(8*'=', relid))
-query = ("SELECT inhparent FROM pg_catalog.pg_inherits WHERE inhrelid = '%s'" %
-    relid)
-sys.stderr.write('{}\n'.format(query))
+query = (
+    "SELECT inhparent FROM pg_catalog.pg_inherits WHERE inhrelid = '{}'".format(
+    relid))
+sys.stderr.write('get_inhparent: {}\n'.format(query))
 rec = plpy.execute(query)
 try:
    return rec[0]['inhparent']
 except:
    return 0
 $$
-    LANGUAGE plpythonu;
-
-
-ALTER FUNCTION get_inhparent(integer) OWNER TO postgres;
+LANGUAGE plpythonu;
 
 --
--- Name: get_pk_fields(oid); Type: FUNCTION; Schema: public; Owner: postgres
+--
 --
 
-CREATE FUNCTION get_pk_fields(oid) RETURNS text
-    AS $$
+CREATE FUNCTION get_pk_fields(oid) RETURNS text AS $$
 """
 retourne les noms des champs constituant la clef primaire de la table
 dont l'oid est passé en référence. Les champs sont ordonnés par "attnum"
@@ -161,7 +128,7 @@ sys.stderr.write("{} get_pk_fields({})\n".format(8*'=', oid))
 rec_st = plpy.execute(
    """SELECT schemaname, relname 
       FROM pg_catalog.pg_stat_all_tables
-      WHERE relid = %s""" % oid)
+      WHERE relid = {}""".format(oid))
 schemaname = rec_st[0]['schemaname']
 relname = rec_st[0]['relname']
 rec_pk_fieldnames = plpy.execute(
@@ -170,11 +137,11 @@ rec_pk_fieldnames = plpy.execute(
       WHERE pa.attrelid IN (
         SELECT oid 
         FROM pg_class 
-        WHERE relname = '%s' 
+        WHERE relname = '{}' 
         AND relnamespace = (
           SELECT oid 
           FROM pg_catalog.pg_namespace 
-          WHERE nspname = '%s')
+          WHERE nspname = '{}')
        ) 
         AND pa.atttypid = pt.oid
         AND pa.attnum = ANY (
@@ -185,9 +152,9 @@ rec_pk_fieldnames = plpy.execute(
           AND conrelid = (
             SELECT relid
             FROM pg_catalog.pg_stat_all_tables
-            WHERE schemaname = '%s'
-            AND relname = '%s')) ]
-       )""" % (relname, schemaname, schemaname, relname))
+            WHERE schemaname = '{}'
+            AND relname = '{}')) ]
+       )""".format(relname, schemaname, schemaname, relname))
 l_fieldnames = []
 for rec in rec_pk_fieldnames:
   l_fieldnames.insert(0, rec['attname'])
@@ -197,10 +164,7 @@ resultat = fqtn + ":" + fieldnames
 sys.stderr.write("{}\n".format(resultat))
 return resultat
 $$
-    LANGUAGE plpythonu;
-
-
-ALTER FUNCTION get_pk_fields(oid) OWNER TO postgres;
+LANGUAGE plpythonu;
 
 ------
 ------ usage
@@ -210,46 +174,21 @@ create table parent(
    a text primary key
 );
 
---
--- Name: before_insert_tuple; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER before_insert_tuple
-    BEFORE INSERT ON parent
+CREATE TRIGGER check_pk
+    BEFORE INSERT or update ON parent
     FOR EACH ROW
-    EXECUTE PROCEDURE before_insert_tuple();
+    EXECUTE PROCEDURE check_pk();
 
 --
--- Name: before_update_tuple; Type: TRIGGER; Schema: public; Owner: postgres
 --
-
-CREATE TRIGGER before_update_tuple
-    BEFORE UPDATE ON parent
-    FOR EACH ROW
-    EXECUTE PROCEDURE before_update_tuple();
-
-
+--
 
 create table child(
    b text,
    primary key(a, b)
 ) inherits(parent);
 
---
--- Name: before_insert_tuple; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER before_insert_tuple
-    BEFORE INSERT ON child
+CREATE TRIGGER check_pk
+    BEFORE INSERT or update ON child
     FOR EACH ROW
-    EXECUTE PROCEDURE before_insert_tuple();
-
---
--- Name: before_update_tuple; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER before_update_tuple
-    BEFORE UPDATE ON child
-    FOR EACH ROW
-    EXECUTE PROCEDURE before_update_tuple();
-
+    EXECUTE PROCEDURE check_pk();
