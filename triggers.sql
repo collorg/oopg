deaad556e5b63f6bd4d663eaf7d1f88a7638f75b
Joël Maïzi <joel.maizi@lirmm.fr>
2015-12-09 12:21:23 +0100

    Exit la table tuple_uid.

--- triggers.sql
+++ triggers.sql
@@ -1,75 +1,22 @@
 create language plpythonu;
--- create extension uuid;
 
 --
--- Name: tuple_uid; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
 --
 
-CREATE TABLE tuple_uid (
-    uid text NOT NULL,
-    fqtn text NOT NULL,
-    changeset integer DEFAULT 0,
-    CONSTRAINT tuple_uid_uid_check CHECK ((uid ~~ '________________________________________________________________________________'::text))
-);
-
-
---
--- Name: after_delete_tuple(); Type: FUNCTION; Schema: public; Owner: postgres
---
-
-CREATE FUNCTION after_delete_tuple() RETURNS trigger
-    AS $$
-
-oid = TD['relid']
-uid = TD['old']['tpl_uid']
-fqtn = plpy.execute("""SELECT get_fqtn('%s')""" %
-   oid)[0]['get_fqtn']
-try:
-   plpy.execute(
-      """DELETE FROM tuple_uid WHERE uid = '%s' and fqtn = '%s'""" % (
-      uid, fqtn))
-except:
-   pass
-$$
-    LANGUAGE plpythonu;
-
-
-ALTER FUNCTION after_delete_tuple() OWNER TO postgres;
-
---
--- Name: before_insert_tuple(); Type: FUNCTION; Schema: public; Owner: postgres
---
-
-CREATE FUNCTION before_insert_tuple() RETURNS trigger
-    AS $$
-
+CREATE FUNCTION before_insert_tuple() RETURNS trigger AS $$
 oid = TD['relid']
 fqtn = plpy.execute("""SELECT get_fqtn('%s')""" % oid)[0]['get_fqtn']
 GD['td'] = TD
-rec = plpy.execute(
-   """SELECT check_uid('%s', '%s', '%s')""" % (oid, fqtn, oid))
-new_uid = rec[0]['check_uid']
-try:
-   TD['new']['tpl_uid'] = new_uid
-   TD['new']['tpl_fqtn'] = fqtn
-   plpy.execute(
-      """INSERT INTO tuple_uid (uid, fqtn) VALUES ('%s', '%s')""" % (
-      new_uid, fqtn))
-   return 'MODIFY'
-except:
-   return 'SKIP'
+plpy.execute("SELECT check_uid('%s', '%s', '%s')" % (oid, fqtn, oid))
 $$
-    LANGUAGE plpythonu;
-
-
-ALTER FUNCTION before_insert_tuple() OWNER TO postgres;
+LANGUAGE plpythonu;
 
 --
--- Name: before_update_tuple(); Type: FUNCTION; Schema: public; Owner: postgres
+--
 --
 
-CREATE FUNCTION before_update_tuple() RETURNS trigger
-    AS $$
+CREATE FUNCTION before_update_tuple() RETURNS trigger AS $$
 from mx import DateTime
 
 oid = TD['relid']
@@ -77,33 +24,15 @@ old_uid = TD['old']['tpl_uid']
 fqtn = TD['old']['tpl_fqtn']
 changeset = TD['old']['tpl_changeset'] + 1
 GD['td'] = TD
-rec = plpy.execute(
-   """SELECT check_uid('%s', '%s', '%s')""" % (
-      oid, fqtn, oid))
-new_uid = rec[0]['check_uid']
-try:
-   TD['new']['tpl_uid'] = new_uid
-   TD['new']['tpl_fqtn'] = fqtn
-   TD['new']['tpl_modification_date'] = DateTime.now()
-   TD['new']['tpl_changeset'] = changeset
-   plpy.execute(
-      "UPDATE tuple_uid SET uid = '%s', changeset = '%s' WHERE uid = '%s'" % (
-      new_uid, changeset, old_uid))
-   return 'MODIFY'
-except:
-   return 'SKIP'
+rec = plpy.execute("SELECT check_uid('%s', '%s', '%s')" % (oid, fqtn, oid))
 $$
-    LANGUAGE plpythonu;
-
-
-ALTER FUNCTION before_update_tuple() OWNER TO postgres;
+LANGUAGE plpythonu;
 
 --
 -- Name: check_uid(integer, text, integer); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
-CREATE FUNCTION check_uid(integer, text, integer) RETURNS text
-    AS $$
+CREATE FUNCTION check_uid(integer, text, integer) RETURNS text AS $$
 """
 recherche d''éventuelles correspondances dans l'arbre d''héritage.
 Si une correspondance est trouvée, déclenche une erreur.
@@ -123,13 +52,12 @@ sys.stderr.write("{} check_uid({}, {}, {})\n".format(
 TD = GD['td']
 sys.stderr.write("GD['td'] = {}\n".format(TD))
 parent_oid = plpy.execute(
-   """SELECT get_inhparent('%s')
-   """ % oid)[0]['get_inhparent']
+    "SELECT get_inhparent('%s')" % oid)[0]['get_inhparent']
 sys.stderr.write("oid du parent {}\n".format(parent_oid))
 if parent_oid:
    # si la table hérite d''une autre table, on recherche l''existence
    # d''un tuple correspondant dans la table mère
-   query = ("""SELECT check_uid('%s', '%s', '%s')""" % (
+   query = ("SELECT check_uid('%s', '%s', '%s')" % (
       oid_orig, fqtn_orig, parent_oid))
    sys.stderr.write("check uid request: {}\n".format(query))
    rec = plpy.execute(query)
@@ -142,17 +70,6 @@ clause = []
 if not pk_fieldnames:
    return new_uid
 pklist = pk_fieldnames.split(',')
-
-if TD['event'] == 'UPDATE':
-   # on aura besoin de vérifier que s'il y a quelque chose
-   # qui a changé dans la clef
-   to_check = False
-   for field in pklist:
-      if TD['old'][field] != TD['new'][field]:
-         # quelque chose a changé dans la clef
-	 to_check = True
-   if not to_check:
-      return TD['old']['tpl_uid']
 
 # construction de la clause pour la requête SELECT
 l_pk_val = []
@@ -174,23 +91,9 @@ sys.stderr.write("check_uid: {}\n".format(req))
 if len(plpy.execute(req)) == 1:
     plpy.error("clef dupliquee")
 
-# le calcul (dans les deux sens) de la clef
-pk_val = ']-+#=-['.join(l_pk_val)
-if len(l_pk_val) > 1:
-   pk_val = '[' + pk_val + ']'
-l_pk_val = pk_val + "." + fqtn
-r_pk_val = fqtn + "." + pk_val
-uid_text = l_pk_val
-l_uid_val = sha.sha(l_pk_val).hexdigest()
-r_uid_val = sha.sha(r_pk_val).hexdigest()
-uid_val = l_uid_val + r_uid_val
-if uid_val:
-   new_uid = uid_val
-# seule le dernier uid est pris en compte.
-return new_uid
+return 'ok'
 $$
-    LANGUAGE plpythonu;
-
+LANGUAGE plpythonu;
 
 ALTER FUNCTION check_uid(integer, text, integer) OWNER TO postgres;
 
@@ -304,8 +207,6 @@ ALTER FUNCTION get_pk_fields(oid) OWNER TO postgres;
 ------
 
 create table parent(
-   tpl_uid text unique not null,
-   tpl_fqtn text not null,
    a text primary key
 );
 
@@ -327,17 +228,9 @@ CREATE TRIGGER before_update_tuple
     FOR EACH ROW
     EXECUTE PROCEDURE before_update_tuple();
 
---
--- Name: after_delete_tuple; Type: TRIGGER; Schema: public; Owner: postgres
---
 
-CREATE TRIGGER after_delete_tuple
-    AFTER DELETE ON parent
-    FOR EACH ROW
-    EXECUTE PROCEDURE after_delete_tuple();
 
 create table child(
-   tpl_uid text unique not null,
    b text,
    primary key(a, b)
 ) inherits(parent);
@@ -360,11 +253,3 @@ CREATE TRIGGER before_update_tuple
     FOR EACH ROW
     EXECUTE PROCEDURE before_update_tuple();
 
---
--- Name: after_delete_tuple; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER after_delete_tuple
-    AFTER DELETE ON child
-    FOR EACH ROW
-    EXECUTE PROCEDURE after_delete_tuple();
